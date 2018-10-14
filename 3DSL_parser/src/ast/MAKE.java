64094c2f353efe267f474a2c1ff29f8e2f11591b
HGZHGZH <HGZHGZH@users.noreply.github.com>
2018-10-14 01:52:02 -0700

    added make command

--- 3DSL_parser/src/ast/MAKE.java
+++ 3DSL_parser/src/ast/MAKE.java
@@ -2,22 +2,45 @@ package ast;
 
 import ui.Main;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 
 public class MAKE extends STATEMENT {
+    private String object;
     private String name;
+    private Map<String, String> propertyMap = new LinkedHashMap<String, String>();
 
     @Override
     public void parse() {
-        tokenizer.getAndCheckNext("new");
-        name = tokenizer.getNext();
+        tokenizer.getAndCheckNext("make");
+        String aOrAn = tokenizer.getNext();
+        if(aOrAn.equals("a") || aOrAn.equals("an")) {
+            object = tokenizer.getNext();
+            tokenizer.getAndCheckNext("called");
+            name = tokenizer.getNext();
+        }
+        String with = tokenizer.getNext();
+        if(with.equals("with:")) {
+            while (tokenizer.checkToken(Main.NEWLINE)){
+                tokenizer.getAndCheckNext(Main.NEWLINE);
+                String property = tokenizer.getNext();
+                String value = tokenizer.getNext();
+                if (value.charAt(0) == '('){
+                    // todo: convert to vector
+                }
+                propertyMap.put(property, value);
+            }
+        }
+        System.out.println(propertyMap);
     }
 
     @Override
     public String evaluate() throws FileNotFoundException, UnsupportedEncodingException {
-        System.out.println("Putting "+this.name+" into symbol table");
-        Main.symbolTable.put(name,"");
+//        System.out.println("Putting "+this.name+" into symbol table");
+//        Main.symbolTable.put(name,"");
         return null;
     }
 
