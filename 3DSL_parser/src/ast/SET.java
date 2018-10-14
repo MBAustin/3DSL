6cca71a0c907014c8c6400e160d7ad7f56aad012
HGZHGZH <HGZHGZH@users.noreply.github.com>
2018-10-13 21:35:53 -0700

    added set command

--- 3DSL_parser/src/ast/SET.java
+++ 3DSL_parser/src/ast/SET.java
@@ -6,19 +6,26 @@ import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 
 public class SET extends STATEMENT {
-    String name;
+    String property;
+    String object;
     String value;
     @Override
     public void parse() {
         tokenizer.getAndCheckNext("set");
-        name = tokenizer.getNext();
+        property = tokenizer.getNext();
+        tokenizer.getAndCheckNext("of");
+        object = tokenizer.getNext();
+        tokenizer.getAndCheckNext("to");
         value = tokenizer.getNext();
+        if(tokenizer.checkToken(Main.NEWLINE)) {
+            tokenizer.getAndCheckNext(Main.NEWLINE);
+        }
     }
 
     @Override
     public String evaluate() throws FileNotFoundException, UnsupportedEncodingException {
-        System.out.println("Setting "+name+" to "+value);
-        Main.symbolTable.put(name,value);
+//        System.out.println("Setting "+name+" to "+value);
+//        Main.symbolTable.put(name,value);
         return null;
     }
 }
