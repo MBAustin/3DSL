7be8d3b99cf66a6cf0d069dc171dadb12cf9b8ac
Geordie Parappilly <geordie.p@hotmail.com>
2018-10-15 15:28:04 -0700

    clone partially done - did implement iterations

--- 3DSL_parser/src/ast/CLONE.java
+++ 3DSL_parser/src/ast/CLONE.java
@@ -6,19 +6,23 @@ import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 
 public class CLONE extends STATEMENT {
+    private String object;
     private String name;
+    private Number iterations;
+    private String step;
 
     @Override
     public void parse() {
-        tokenizer.getAndCheckNext("new");
+        tokenizer.getAndCheckNext("clone");
+        object = tokenizer.getNext();
+        tokenizer.getAndCheckNext("as");
         name = tokenizer.getNext();
     }
 
     @Override
     public String evaluate() throws FileNotFoundException, UnsupportedEncodingException {
-        System.out.println("Putting "+this.name+" into symbol table");
-        Main.symbolTable.put(name,"");
-        return null;
+        //System.out.println("cmds.duplicate('"+object+"', name='"+name+"')");
+        return "cmds.duplicate('"+object+"', name='"+name+"')";
     }
 
     public String getName(){return name;}
