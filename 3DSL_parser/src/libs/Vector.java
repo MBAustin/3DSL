b44995f4a2876b13b0f108173b02e895d2bc6285
Reed <reedesler+github@gmail.com>
2018-10-15 14:25:12 -0700

    Moved vector from string function

--- 3DSL_parser/src/libs/Vector.java
+++ 3DSL_parser/src/libs/Vector.java
@@ -1,10 +1,22 @@
 package libs;
 
+import java.util.Arrays;
+
 public class Vector {
     public float a,b,c;
     public Vector(float a, float b, float c) {
         this.a = a;
         this.b = b;
         this.c = c;
+    }
+
+    public static Vector fromString(String str) {
+        String[] numbers = str.split("[(,)]");
+        if (numbers.length != 4) {
+            System.out.println("FAILED: Got wrong amount of numbers in vector: " + str + ";  " + Arrays.toString(numbers));
+            System.exit(0);
+        }
+        System.out.println("got vector " + str + " as " + Arrays.toString(numbers));
+        return new Vector(Float.parseFloat(numbers[1]), Float.parseFloat(numbers[2]), Float.parseFloat(numbers[3]));
     }
 }
