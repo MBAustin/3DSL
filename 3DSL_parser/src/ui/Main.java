e7cc06c6723eeaea1c5fe52907c9b8dc9297e9b9
mopok <markrules@shaw.ca>
2018-10-16 12:24:50 -0700

    recursively check symbol table

--- 3DSL_parser/src/ui/Main.java
+++ 3DSL_parser/src/ui/Main.java
@@ -35,11 +35,23 @@ public class Main {
     }
 
     public static String getValue(String str) {
-        return Main.symbolTable.containsKey(str) ? (String) Main.symbolTable.get(str) : str;
+        // Recursively check
+        if (Main.symbolTable.containsKey(str)) {
+            return getValue((String) Main.symbolTable.get(str));
+        } else{
+            return str;
+        }
     }
 
     public static Vector getVector(Vector vector) {
-        return vector.variable == null ? vector : Vector.fromString((String) Main.symbolTable.get(vector.variable));
+        if (vector.variable == null) {
+            return vector;
+        } else {
+            return Main.getVector(Vector.fromString((String) Main.symbolTable.get(vector.variable)));
+        }
+
+
+//        return vector.variable == null ? vector : Vector.fromString((String) Main.symbolTable.get(vector.variable));
     }
 
 }
