7780a758a40da48d99578d6afa90df993cc40a3a
Reed <reedesler+github@gmail.com>
2018-10-15 17:19:06 -0700

    Added store statement

--- 3DSL_parser/src/ast/STORE.java
+++ 3DSL_parser/src/ast/STORE.java
@@ -6,18 +6,21 @@ import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 
 public class STORE extends STATEMENT {
+    private String value;
     private String name;
 
     @Override
     public void parse() {
-        tokenizer.getAndCheckNext("new");
+        tokenizer.getAndCheckNext("store");
+        value = tokenizer.getNext();
+        tokenizer.getAndCheckNext("as");
         name = tokenizer.getNext();
     }
 
     @Override
     public String evaluate() throws FileNotFoundException, UnsupportedEncodingException {
         System.out.println("Putting "+this.name+" into symbol table");
-        Main.symbolTable.put(name,"");
+        Main.symbolTable.put(name,value);
         return null;
     }
 
